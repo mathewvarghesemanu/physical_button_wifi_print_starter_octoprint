9121c9c4b7f34a927be8eb3f7e1207b4a2bb14a8
Mathew Varghese <mathewvarghesemanu@gmail.com>
2020-04-29 22:37:45 +0530

    Update easywifiswitch.ino

--- Code/easywifiswitch/easywifiswitch.ino
+++ Code/easywifiswitch/easywifiswitch.ino
@@ -1,14 +1,29 @@
+/*
+physical_button_wifi_print_starter_octoprint
+
+Easily start 3D printss using a physical button connected to an ESP32
+
+Switch connected to pin 13
+
+Created 29-04-2020
+By Mathew Varghese
+
+https://github.com/mathewvarghesemanu/physical_button_wifi_print_starter_octoprint
+
+*/
+
+
 #include <ArduinoJson.h>
 #include <HTTPClient.h>
 #include <WiFiMulti.h>
 
-const char *AP_SSID = "manu";
-const char *AP_PWD = "ambalathil1234";
+const char *AP_SSID = "manu"; //change ssid
+const char *AP_PWD = "ambalathil1234"; //change password
 
 WiFiMulti wifiMulti;
 void postDataToServer();
 void setup() {
-  pinMode(13, INPUT_PULLUP);
+  pinMode(13, INPUT_PULLUP); 
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(9600);
 
@@ -19,11 +34,11 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
   if (digitalRead(13) != 1)//if switch is pressed, led lits up, command sent and delay for 1.5 minutes
   {
     Serial.print("on\n");
-    digitalWrite(LED_BUILTIN, 1);
+    digitalWrite(LED_BUILTIN, 1); //internal LED is lit up untill the server returns a value
     postDataToServer();
     digitalWrite(LED_BUILTIN, 0);
     delay(500);
@@ -49,13 +64,13 @@ void postDataToServer() {
     http.addHeader("X-Api-Key", "401D82B1BD4B47D6817DBD5C8A6AD297");
     StaticJsonDocument<200> doc;
     // Add values in the document
-    //
+    
     doc["command"] = "start";
 
     String requestBody;
     serializeJson(doc, requestBody);
 
-    int httpResponseCode = http.POST(requestBody);
+    int httpResponseCode = http.POST(requestBody); //send command to start print to octopi server
 
     if (httpResponseCode > 0) {
       Serial.print(httpResponseCode);
